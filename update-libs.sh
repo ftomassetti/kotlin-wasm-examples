7042cd54ef233978926a002c67b72625eecd09f5
Federico Tomassetti <federico@tomassetti.me>
2018-02-24 13:49:10 +0100

    updating libraries

---
+++ update-libs.sh
@@ -0,0 +1,2 @@
+jsinterop -pkg kotlinx.interop.wasm.dom -o lib/dom -target wasm32 && rm -Rf lib/dom-build
+jsinterop -pkg kotlinx.interop.wasm.math -o lib/math -target wasm32 && rm -Rf lib/math-build
